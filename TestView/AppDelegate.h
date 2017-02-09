6daa62950339503699d7e4f60fdf8b700fbb23fa
romanvintish <romanvintish@gmail.com>
2017-02-09 10:56:35 +0200

    Initial Commit

---
+++ TestView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestView
+//
+//  Created by VINTISH ROMAN on 2/9/17.
+//  Copyright © 2017 Roman Vintish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestView
+//
+//  Created by VINTISH ROMAN on 2/9/17.
+//  Copyright © 2017 Roman Vintish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
